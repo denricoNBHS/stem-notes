57a84291fcb3ddcdc02d956a0ffb79208d62792d
Dave Enrico <denrico@northbergen.k12.nj.us>
2016-07-16 20:50:37 -0400

    Tidy up directory structure and add exercise 1

---
+++ exercises/exercise_1/exercise.py
@@ -0,0 +1,7 @@
+# In the space below, create a variable named one that stores the integer 1.
+# Then create a variable named roman_one that stores the string "I"
+# Finally, write code that tests whether one and roman_one are equivalent.
+
+
+
+# --- Enter your code below --- #
---
+++ exercises/exercise_1/test.py
@@ -0,0 +1,25 @@
+# This code tests your solutions. Please do not change it! #
+import re
+from unittest import TestCase
+
+from exercise_1 import exercise
+
+
+class Test(TestCase):
+    def test_vars(self):
+        self.assertTrue(hasattr(exercise, "one"),
+                        'You must declare a variable called "one".')
+        self.assertEqual(exercise.one, 1, 'variable one\'s value seems off.')
+
+        self.assertTrue(hasattr(exercise, "roman_one"),
+                        'Did you declare the variable "roman_one"?')
+        self.assertEqual(exercise.roman_one, "I",
+                         'roman_one is storing the wrong value.')
+
+        with open('exercise.py') as f:
+            s = f.read()
+            m = re.search('(?P<var1>one|roman_one) *== *(?P<var2>one|roman_one)', s)
+
+            self.assertTrue(m, 'Did you test for the right equivalence?')
+            self.assertNotEqual(m.group('var1'), m.group('var2'),
+                                'Seeing if a variable is equivalent to itself is silly!')
